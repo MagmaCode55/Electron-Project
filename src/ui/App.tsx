0a909e94a71096d9f79a9d6fc469d7339512dd54
Vishag <vishagraghav@gmail.com>
2025-03-19 14:02:58 -0400

    change 1

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(()=>{
+    // @ts-ignore
+    window.electron.subscribeStatistics(stats=>console.log(stats));
+  },[]);
 
   return (
     <>
@@ -12,6 +17,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        
       </div>
       <h1>Vite + React</h1>
       <div className="card">
